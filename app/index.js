2dc0b5ec373760adf891861f56f68ba941ced115
Loren Norman <lorennorman@gmail.com>
2022-11-22 17:34:15 -0600

    topic-based message parsing

--- app/index.js
+++ app/index.js
@@ -1,4 +1,47 @@
-import { flatten, map, uniq } from "lodash-es"
+import { forEach, flatten, map, sortBy, uniq } from "lodash-es"
+
+import PB from '../protobufs'
+
+const TOPIC_PARSERS = {
+  "*/wprsnpr/info/status": PB.CreateDescriptionRequest,
+  "*/info/status/device/complete": PB.RegistrationComplete,
+  "*/signals/device": PB.CreateSignalRequest,
+  "*/signals/device/i2c": PB.I2CResponse,
+  "*/signals/device/pinConfigComplete": PB.SignalResponse,
+  "*/signals/device/servo": PB.ServoResponse,
+  "*/signals/device/ds18x20": PB.Ds18x20Response,
+  "*/signals/device/pwm": PB.PWMResponse,
+}
+
+const parseMessageByTopic = (topic, message) => {
+  let parser
+
+  forEach(TOPIC_PARSERS, (currentParser, topicSpec) => {
+    if(parser) { return }
+
+    if(topicSpec.startsWith('*')) {
+      const topicEndFragment = topicSpec.slice(1)
+      if(topic.endsWith(topicEndFragment)) {
+        parser = currentParser
+      }
+
+    } else {
+      if(topic === topicSpec) {
+        parser = currentParser
+      }
+    }
+  })
+
+  if(!parser) {
+    try{
+      return JSON.parse(message)
+    } catch(err) { return message }
+  }
+
+  return (parser.deserializeBinary
+    ? parser.deserializeBinary(message).toObject()
+    : parser(message))
+}
 
 // from react
 const options = {
@@ -30,15 +73,20 @@ const
   subscriptions = []
 
 client.on('message', (topic, message) => {
-  messages.unshift({ topic, message })
+  const parsedMessage = parseMessageByTopic(topic, message)
+  messages.unshift({ topic, message, parsedMessage })
   refreshMessages()
+
+  // if it's a broker introspection topic, update the data stores
   if(topic === "state/clients") {
     const clientsCollection = JSON.parse(message)
+
     clients.splice(0, Infinity, ...map(clientsCollection, "id"))
-    subscriptions.splice(0, Infinity, ...uniq(flatten(map(clientsCollection, "subscriptions"))))
     console.log("Clients:", clients)
-    console.log("Subscriptions:", subscriptions)
     refreshClients()
+
+    subscriptions.splice(0, Infinity, ...sortBy(uniq(flatten(map(clientsCollection, "subscriptions")))))
+    console.log("Subscriptions:", subscriptions)
     refreshSubscriptions()
   }
 })
@@ -47,8 +95,8 @@ const refreshMessages = () => {
   document.getElementsByClassName('messages')[0].innerHTML = messages.map(message => `
     <div>
       <dl>
-        <dt>Topic:</dt> <dd title="${ message.topic }">${ parseTopic(message.topic) }</dd>
-        <dt>Payload:</dt> <dd title='${ message.message }'>${ parseMessage(message.message) }</dd>
+        <dt>Topic:</dt> <dd title="${ message.topic }">${ renderTopic(message.topic) }</dd>
+        <dt>Payload:</dt> <dd title='${ message.message }'>${ renderMessage(message.parsedMessage) }</dd>
       </dl>
     </div>
   `).join("<hr />")
@@ -66,7 +114,7 @@ const refreshSubscriptions = () => {
   `).join('')
 }
 
-const parseTopic = topic => {
+const renderTopic = topic => {
   if(topic.startsWith("$SYS")) {
     const action = topic.split('/').slice(2)
     return `$SYS/.../${action.join('/')}`
@@ -75,14 +123,8 @@ const parseTopic = topic => {
   return topic
 }
 
-const parseMessage = message => {
-  if(!message) { return message }
-
-  try {
-    message = JSON.parse(message)
-  } catch(error) {}
-
-  // try protobufs, etc
-
-  return JSON.stringify(message, null, 2)
+const renderMessage = message => {
+  return (message
+    ? JSON.stringify(message, null, 2)
+    : message)
 }
