6949d19309419628a88140ccff03dcba28bf3800
Loren Norman <lorennorman@gmail.com>
2022-11-26 22:45:12 -0600

    switch to a protobufjs service wrapper

---
+++ app/protobuf_service.js
@@ -0,0 +1,62 @@
+import { filter, flatMap, groupBy, includes, map, reduce, reject, sortBy } from 'lodash-es'
+import { ref, computed } from 'vue'
+
+// make sure we have the global protobufjs object
+if(!window.protobuf) {
+  throw new Error("protobufjs library not loaded")
+}
+
+// track whether we've loaded proto files
+const protobufRoot = ref(null)
+// extract primitive types
+const PRIMITIVE_TYPES = Object.keys(protobuf.types.basic)
+
+export const
+  loadProtoFile = async filePath => {
+    protobufRoot.value = await protobuf.load(filePath)
+  },
+
+  allProtos = computed(() => {
+    if(!protobufRoot.value) { return [] }
+
+    const
+      // dig out the leaf nodes of the wippersnapper protos
+      modules = protobufRoot.value.nested.wippersnapper.nested,
+      messages = flatMap(
+        map(modules, "nested.v1.nested"),
+        Object.values
+      ),
+      // reject deprecated messages
+      liveMessages = reject(messages, "options.deprecated"),
+      sortedMessages = sortBy(liveMessages, "name")
+
+    return sortedMessages
+  }),
+
+  protosByModule = computed(() => groupBy(allProtos.value, "filename")),
+
+  fields = messageToQuery => {
+    // look at its fields
+    const
+      fields = Object.values(messageToQuery.fields),
+      // pull out all primitives
+      primitives = filter(fields, ({ type }) => includes(PRIMITIVE_TYPES, type)),
+      // pull out oneofs
+      { oneofs } = messageToQuery,
+      { messages, enums } = reduce(fields, ({ messages, enums }, field) => {
+        const oneofNames = map(flatMap(oneofs, "fieldsArray"), "name")
+        if(includes(oneofNames, field.name) || includes(PRIMITIVE_TYPES, field.type)) {
+          return { messages, enums }
+        }
+
+        const typeOrEnum = protobufRoot.value.lookup(field.type)
+        typeOrEnum.fieldName = field.name
+
+        return typeOrEnum.values
+          ? { messages, enums: enums.concat(typeOrEnum) }
+          : { messages: messages.concat(typeOrEnum), enums }
+      }, { messages: [], enums: [] })
+
+    // lookup the remainder and sort enums and messages
+    return { oneofs, messages, enums, primitives }
+  }
--- app/stores/message.js
+++ app/stores/message.js
@@ -1,37 +1,12 @@
-import { filter, flatMap, includes, map, reject } from 'lodash-es'
-import { ref, computed } from "vue"
+import { ref } from "vue"
 import { defineStore } from "pinia"
 import { useUIStore } from './ui'
 
-const PRIMITIVE_TYPES = [
-  "int32", "uint32", "float", "string", "bool"
-]
 export const useMessageStore = defineStore('message', () => {
-  const
-    message = ref(null),
-    fields = computed(() => Object.values(message.value.fields)),
-    oneofs = computed(() => message.value.oneofs),
-    primitives = computed(() => (
-      filter(fields.value, field => includes(PRIMITIVE_TYPES, field.type))
-    )),
-    rejectPrimitivesAndOneofs = fields => {
-      const oneofNames = map(flatMap(oneofs.value, "fieldsArray"), "name")
-      return reject(fields, field => (
-        includes(PRIMITIVE_TYPES, field.type) || includes(oneofNames, field.name)
-      ))
-    },
-    enums = computed(() => reject(rejectPrimitivesAndOneofs(fields.value), field => field.fields)),
-    messages = computed(() => reject(rejectPrimitivesAndOneofs(fields.value), field => !field.fields))
-
   return {
-    message,
-    oneofs,
-    messages,
-    enums,
-    primitives,
-    newMessage: function(protobuf) {
-      console.log(protobuf)
-      this.message = protobuf
+    message: ref(null),
+    newMessage: function(message) {
+      this.message = message
       useUIStore().setMode('configureMessage')
     }
   }
