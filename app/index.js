9e1d60531d060b1444855d134339fcfa596e1150
Loren Norman <lorennorman@gmail.com>
2024-03-13 17:21:13 -0400

    remove the service worker

--- app/index.js
+++ app/index.js
@@ -13,15 +13,6 @@ import { loadStoreData } from './stores/data_loader'
 import { loadProtoFile } from './protobuf_service'
 import { connect } from './mqtt_service'
 
-// fix an xhr error after the first load
-navigator.serviceWorker.register('fetch_headers_service_worker.js')
-  .then(registration => {
-    console.log('Service worker registered with scope: ', registration.scope)
-  })
-  .catch(err => {
-    console.log('ServiceWorker registration failed: ', err);
-  })
-
 // once page is loaded...
 document.addEventListener("DOMContentLoaded", async () => {
   await loadStoreData()
