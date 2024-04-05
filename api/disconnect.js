304d96a155d53270c285c6586d95cd31897fc4f3
Loren Norman <lorennorman@gmail.com>
2024-04-05 01:49:53 -0500

    fix disconnect command and timing

--- api/disconnect.js
+++ api/disconnect.js
@@ -4,13 +4,13 @@ export default (router, broker) => {
   console.log("Installing Disconnect Command")
 
   // Disconnect a connected client by id
-  router.post('/disconnect', ({ protomq: { client }}, res) => {
+  router.post('/disconnect', ({ protomq: { client, clientId }}, res) => {
     if(!client) {
-      res.json({ status: 'ERROR', message: `No client found with id: ${client.id}` })
+      res.json({ status: 'ERROR', message: `No client found with id: ${clientId}` })
       return
     }
 
-    client.close(res.json({ status: 'OK' }))
+    client.close(() => res.json({ status: 'OK' }))
     console.log(`Client "${client.id} disconnected by api`)
   })
 }
